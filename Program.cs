68660af6fa90690e17ebaf2aa41f56e1cbb81882
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 03:42:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int a, b;
+ 
+        Console.WriteLine("-9,-3");
+ 
+        a = -9;
+        b =-3;
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }

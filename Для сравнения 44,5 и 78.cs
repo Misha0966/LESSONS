9d9b24b00cca8bf9dd932d7b52eaf26dd4d52bfd
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 05:30:37 +0300

    Add files via upload
    Для поиска максимального из этих 3-х

---
+++ Для сравнения 44,5 и 78.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int num1, num2, num3;
+ 
+        Console.WriteLine("44,5,78");
+ 
+        num1 = 44;
+        num2 = 5;
+        num3 = 78;
+      
+        if (num1 > num2)
+ 
+        {
+ 
+            Console.WriteLine("44");
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("3");
+ 
+        }
+   
+    if (num2 > num1)
+ {
+ 
+            Console.WriteLine("3");
+ 
+        }
+else
+ 
+        {
+ 
+            Console.WriteLine("5");
+ 
+        }
+    if (num3>num2)
+{
+ Console.WriteLine("{0} наибольшее число ", num3);
+}
+
+else
+
+{
+     Console.WriteLine("{0} наибольшее число ", num2);
+}
+
+
+ Console.ReadLine();
+    }

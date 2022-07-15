8acdbc6d0a38603e52d8062ea9c32f46e13f7ece
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 04:04:48 +0300

    Add files via upload
    Для чисел 2 и 10

---
+++ Program2-10.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int a, b;
+ 
+        Console.WriteLine("2,10");
+ 
+        a = 2;
+        b =10;
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }

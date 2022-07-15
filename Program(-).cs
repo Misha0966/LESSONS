c453454fe1d523b06ced78524d9f53b37b358de7
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 03:54:05 +0300

    Add files via upload
    Для отрицательных чисел

---
+++ Program(-).cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int a, b;
+ 
+        Console.WriteLine("-3,-9");
+ 
+        a = -3;
+        b =-9;
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }

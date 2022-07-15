a49728da0d673995d84dc97f45a7eca753758394
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 05:21:47 +0300

    Add files via upload
    Для поиска максимального из 3-х

---
+++ Программа для сравнения 2,3,7..cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int num1, num2, num3;
+ 
+        Console.WriteLine("2,3,7");
+ 
+        num1 = 2;
+        num2 = 3;
+        num3 = 7;
+      
+        if (num1 < num2)
+ 
+        {
+ 
+            Console.WriteLine("2");
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("3");
+ 
+        }
+   
+    if (num2 > num1)
+ {
+ 
+            Console.WriteLine("3");
+ 
+        }
+else
+ 
+        {
+ 
+            Console.WriteLine("2");
+ 
+        }
+    if (num3>num2)
+{
+ Console.WriteLine("{0} наибольшее число ", num3);
+}
+
+else
+
+{
+     Console.WriteLine("{0} наибольшее число ", num2);
+}
+
+
+ Console.ReadLine();
+    }

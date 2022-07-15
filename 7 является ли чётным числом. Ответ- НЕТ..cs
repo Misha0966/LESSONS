d17f702bdabc4caa1e4862255f42648ee062f002
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 06:08:41 +0300

    Add files via upload
    7-- НЕТ

---
+++ 7 является ли чётным числом. Ответ- НЕТ..cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+  int i;
+ 
+            i = 7;
+ 
+            if (i % 2 == 0)
+ 
+            {
+ 
+                Console.Write("ДА");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("НЕТ");
+ 
+                Console.Read();
+ 
+            }

36449bf5739fc8e677e86b2953f5494a2431fe13
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-07-15 05:43:20 +0300

    Add files via upload
    Для поиска максимального из этих 3-х

---
+++ Для чисел 22, 3, 9.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ 
+    {
+ 
+        int num1, num2, num3;
+ 
+        Console.WriteLine("22,3,9");
+ 
+        num1 = 22;
+        num2 = 3;
+        num3 = 9;
+      
+        if (num1 > num2)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", num1);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", num2);
+ 
+        }
+   
+    if (num2 > num1)
+ {
+ 
+            Console.WriteLine("3333333333");
+ 
+        }
+else
+ 
+        {
+ 
+            Console.WriteLine("3");
+ 
+        }
+    if (num3<num2)
+{
+ Console.WriteLine(1000000);
+}
+
+else
+
+{
+     Console.WriteLine(9);
+}
+
+
+ Console.ReadLine();
+    }
